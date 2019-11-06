c8b523b543ab925a7eea3388881f8eb5ffb77fac
Emmanuel <emmajay099@gmail.com>
2019-11-06 12:00:23 +0000

    Add files via upload

---
+++ die/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    srand(7);
+
+   for (int x=0; x<10; x++)
+   {
+
+       cout << rand()<<endl;
+   }
+
+
+    return 0;
+}
